ee2081aaa06d1925ef31cf737055c64ed2b57875
aminataCmd <ami95190@gmail.com>
2024-01-20 12:46:38 +0100

    authentification + sign up

--- API/routes/usersRoutes.py
+++ API/routes/usersRoutes.py
@@ -1,24 +1,47 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 from API.services import usersServices as service
 from API.routes.utils import getResponse, riseHttpExceptionIfNotFound
-from API.models.users import UserTemp
+from API.models.users import UserTemp, User
 from bson import objectid
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from API.models.token import Token, TokenData
+from datetime import datetime, timedelta
+from API.config.settings import settings
 
 usersRoutes = APIRouter()
-base = "/users/"
+base = "/users"
 _notFoundMessage = "Could not find user with the given Id."
 
 @usersRoutes.get(base)
 async def getAll():
     return await service.getAllUser()
 
-@usersRoutes.get(base+'{id}')
+@usersRoutes.get(base+'/{id}')
 async def getById(id):
     return await resultVerification(id)
 
 @usersRoutes.post(base)
 async def insertUser(userData: UserTemp):
     return await service.insertUser(userData)
+
+@usersRoutes.post(f"{base}/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = service.authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = service.create_access_token(
+        data={"sub": user["pseudo"]}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@usersRoutes.get(f"{base}/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(service.get_current_active_user)):
+    return current_user
 
 async def resultVerification(id: objectid) -> dict:
     result = await service.getById(id)
--- API/services/usersServices.py
+++ API/services/usersServices.py
@@ -1,10 +1,17 @@
 from bson import ObjectId
 from API.config.database import db
-from API.models.users import User, UserInDB, UserTemp
+from API.models.users import UserInDB, UserTemp, User
 from API.schemas.serializeObjects import serializeDict, serializeList
 from passlib.context import CryptContext
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Depends
+from typing import Optional
+from datetime import datetime, timedelta
+from API.config.settings import settings
+from jose import JWTError, jwt
+from API.models.token import TokenData
+from fastapi.security import OAuth2PasswordBearer
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -23,7 +30,7 @@ async def insertUser(userData: UserTemp):
     # check if pseudo already exists
     if db.usersData.find_one({"pseudo": userData.pseudo}):
         raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
+            status_code=409,
             detail="Pseudo already exists",
         )
     hashedPassword = get_password_hash(userData.mdp)
@@ -46,4 +53,52 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password).encode('utf-8')
+    
+def authenticate_user(pseudo: str, password: str):
+    user = db.usersData.find_one({"pseudo": pseudo})
+    if not user:
+        return False
+    if not verify_password(password, user["hashedPassword"]):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, key=settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    print("encoded_jwt:" + encoded_jwt)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        print("token :", token)
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+        print(payload)
+        # TODO : make the decode work
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = db.usersData.find_one({"pseudo":token_data.username})
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if not current_user.isActive:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
